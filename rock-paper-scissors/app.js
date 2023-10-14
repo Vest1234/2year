895c91cf3ee323889fd1f7bcc8989fcd90400145
movavi_school <gaysinvlad3@gmail.com>
2023-10-14 20:14:43 +0700

    rock

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,7 +1,7 @@
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
-const gameElement = [rock, paper, scissors]
+const gameElement = ["rock", "paper", "scissors"]
 
 rock.addEventListener
 
@@ -23,7 +23,12 @@ scissors.addEventListener("click",
 )
 
 
-console.log(Math.random(0,1,2))
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);  
+  }
+  console.log("computer:", gameElement[getRandomInt(3)]);
+
+
 
 
 
