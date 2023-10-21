a392a7ef90ea27ba54e14b471a3552b9053b081e
movavi_school <gaysinvlad3@gmail.com>
2023-10-21 20:27:02 +0700

    Игра в процессе

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,34 +1,52 @@
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
-const gameElement = ["rock", "paper", "scissors"]
+const table = document.querySelector('.score')
+const gameElements = ["rock", "paper", "scissors"]
 
 rock.addEventListener
 
 rock.addEventListener("click", 
     function(){
-        console.log("rock")
+        console.log("Player: rock")
+        console.log("computer:", gameElements[getRandomInt(3)] )
     }
 )
 paper.addEventListener("click",
     function () {
-        console.log("paper")
+        console.log("Player: paper")
+        console.log("computer:", gameElements[getRandomInt(3)] )
     }
   )
 
 scissors.addEventListener("click",
     function () {
-        console.log("scissors")
+        console.log("Player: scissors")
+        console.log("computer:", gameElements[getRandomInt(3)] )
     }
 )
 
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);  
+    
   }
-  console.log("computer:", gameElement[getRandomInt(3)]);
 
 
+function playRound(player, computer) {
+    if(player === rock && computer === scissors) {
+        console.log("player: won")
+    }else if (player === scissors && computer === paper) {
+        console.log("player: won")
+    }else if (player === rock && computer === rock){
+        console.log("Tie")
+    }else if (player === paper && computer === paper){
+        console.log("Tie")
+    }else if (player === scissors && computer === scissors){
+        console.log("Tie")
+    }else {
+        console.log("computer: won")
+    }
 
-
-
+}
+playRound()
