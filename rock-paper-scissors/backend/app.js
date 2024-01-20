ef432fa47fa423958624ce961d707383febaaf9a
movavi_school <gaysinvlad3@gmail.com>
2024-01-20 18:04:19 +0700

    На уроке

--- rock-paper-scissors/backend/app.js
+++ rock-paper-scissors/backend/app.js
@@ -1,21 +1,17 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
 const path = require('node:path')
 const frontend = path.resolve(__dirname, "../frontend")
 const router = require(path.resolve(__dirname, "router.js"))
 const app = express();
+const nunjucks = require('nunjucks')
 
 
-app.engine(
-    '.hbs',
-    handlebars.engine({
-        extnme: '.hbs',
-        defaultLayout: 'main',
-    })
-);
+app.
 app.set('views', `${__dirname}/views`);
-app.set('view engine', '.hbs');
-
+nunjucks.configure(views, {
+    autoescape: true,
+    express: app,
+})
 app.use(router)
 app.use(express.static(frontend))
 
